12402c9304d3e7f9c740ab3839678c65b188b21b
R.Surenthara Kumar <ukrsuren@gmail.com>
2026-07-27 13:22:12 +0530

    Update D.  3(a).Packages And Interfaces With Access  Specifiers .java

--- D.  3(a).Packages And Interfaces With Access  Specifiers .java
+++ D.  3(a).Packages And Interfaces With Access  Specifiers .java
@@ -1,32 +1,93 @@
+public class ShapeAreaCalculator {
+public static void main(String[] args) {
+System.out.println("----- Shape Area Calculator -----\n");
+
+        Circle circle = new Circle(5.0);
+        System.out.println("Circle:");
+        System.out.println("Radius = " + circle.getRadius());
+        System.out.println("Area of Circle = " + circle.calculateArea());
+
+        System.out.println();
+
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        System.out.println("Rectangle:");
+        System.out.println("Length = " + rectangle.getLength() + ", Width = " + rectangle.getWidth());
+        System.out.println("Area of Rectangle = " + rectangle.calculateArea());
+
+        System.out.println();
+
+        Triangle triangle = new Triangle(3.0, 8.0);
+        System.out.println("Triangle:");
+        System.out.println("Base = " + triangle.getBase() + ", Height = " + triangle.getHeight());
+        System.out.println("Area of Triangle = " + triangle.calculateArea());
+    }
+}
 
 interface Shape {
-    double calculateArea();
+    double calculateArea();
 }
+
 class Circle implements Shape {
-    private double radius;
-    Circle(double radius) {
-        this.radius = radius;
-    }
-    public double calculateArea() {
-        return Math.PI * radius * radius;
-    }
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
 }
+
 class Rectangle implements Shape {
-    protected double length, width;
-    Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
-    }
-    public double calculateArea() {
-        return length * width;
-    }
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
 }
+
 class Triangle implements Shape {
-    public double base, height;
-    Triangle(double base, double height) {
-        this.base = base;
-        this.height = height;
-    }
-    public double calculateArea() {
-        return 0.5 * base * height;
-    }
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
+
+
